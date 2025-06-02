f5d08def7a09bfee6cae0aa656a3d0500a94b295
Fuhito Suguri <bitwalk@gmail.com>
2025-06-02 17:37:14 +0900

    update

--- widgets/labels.py
+++ widgets/labels.py
@@ -44,6 +44,7 @@ class PlainTextEdit(QPlainTextEdit):
         super().__init__(*args)
         self.setStyleSheet("""
             QPlainTextEdit {
+                font-family: monospace;
                 border-width: 0;
                 border-style: none;
                 padding: 0;
