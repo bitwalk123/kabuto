9f4103d041e8bfdefaa85412cb7bf575eac801b0
Fuhito Suguri <bitwalk@gmail.com>
2025-06-02 16:56:35 +0900

    update

--- widgets/dialog.py
+++ widgets/dialog.py
@@ -1,7 +1,11 @@
+import os
+
+from PySide6.QtCore import QMargins, Qt
+from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtWidgets import QDialog, QDialogButtonBox
 
 from structs.res import AppRes
-from widgets.labels import LabelRight, LabelLeft, TextEdit
+from widgets.labels import LabelRight, LabelLeft, TextEdit, Label
 from widgets.layouts import GridLayout
 
 
@@ -18,6 +22,12 @@ class DlgAboutThis(QDialog):
         layout.addWidget(lab_name_0, r, 0)
         lab_name_1 = LabelLeft(progname)
         layout.addWidget(lab_name_1, r, 1)
+        lab_name_2 = Label()
+        pixmap = QPixmap(os.path.join(res.dir_image, "kabuto.png")).scaledToWidth(48)
+        lab_name_2.setPixmap(pixmap)
+        lab_name_2.setContentsMargins(QMargins(5, 0, 5, 0))
+        lab_name_2.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_name_2, r, 2, 3, 1)
 
         r += 1
         lab_ver_0 = LabelRight("バージョン")
@@ -40,11 +50,11 @@ class DlgAboutThis(QDialog):
             "RSS により Excel ワークシート上に読み込み、" \
             "さらに python の xlwings のパッケージが読み込んで処理しています。"
         lab_desc_1.setText(msg)
-        layout.addWidget(lab_desc_1, r, 1)
+        layout.addWidget(lab_desc_1, r, 1, 1, 2)
 
         r += 1
         bbox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
         bbox.accepted.connect(self.accept)
-        layout.addWidget(bbox, r, 0, 1, 2)
+        layout.addWidget(bbox, r, 0, 1, 3)
 
-        layout.setColumnStretch(1, 1)
+        layout.setColumnStretch(2, 1)
