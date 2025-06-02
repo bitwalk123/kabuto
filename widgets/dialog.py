44a22d2772ef0fbf4f235dcfb28a98a31061968b
Fuhito Suguri <bitwalk@gmail.com>
2025-06-02 16:02:02 +0900

    update

---
+++ widgets/dialog.py
@@ -0,0 +1,30 @@
+from PySide6.QtWidgets import QDialog, QDialogButtonBox
+
+from structs.res import AppRes
+from widgets.labels import LabelRight, LabelLeft
+from widgets.layouts import GridLayout
+
+
+class DlgAboutThis(QDialog):
+    def __init__(self, res: AppRes, progname: str, progver: str):
+        super().__init__()
+        self.setWindowTitle('このアプリについて')
+
+        layout = GridLayout()
+        self.setLayout(layout)
+
+        r = 0
+        lab_name_0 = LabelRight("アプリ名 ")
+        layout.addWidget(lab_name_0, r, 0)
+        lab_name_1 = LabelLeft(progname)
+        layout.addWidget(lab_name_1, r, 1)
+
+        r += 1
+        lab_ver_0 = LabelRight("バージョン ")
+        layout.addWidget(lab_ver_0, r, 0)
+        lab_ver_1 = LabelLeft(progver)
+        layout.addWidget(lab_ver_1, r, 1)
+
+        bbox = QDialogButtonBox(QDialogButtonBox.StandardButton.Yes)
+        bbox.accepted.connect(self.accept)
+        layout.addWidget(bbox)
