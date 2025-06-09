51076a63c602c47fc35941f922e1d8a04bcecaab
Fuhito Suguri <bitwalk@gmail.com>
2025-06-09 17:40:23 +0900

    update

--- widgets/buttons.py
+++ widgets/buttons.py
@@ -87,7 +87,7 @@ class ButtonSemiAuto(QPushButton):
         self.setStyleSheet("""
             QPushButton {
                 font-size: 7pt;
-                background-color: #ffa500;
+                background-color: #fa8;
             }
         """)
         self.setText("手動エントリ\n自動（強制）返済")
