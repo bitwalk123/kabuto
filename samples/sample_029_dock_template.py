0b5fc00d09cb6d54a00b5923e52a63ad9d1ef35a
Fuhito Suguri <bitwalk@gmail.com>
2025-07-18 08:29:12 +0900

    update

--- samples/sample_029_dock_template.py
+++ samples/sample_029_dock_template.py
@@ -8,22 +8,31 @@ from PySide6.QtWidgets import (
 
 from widgets.containers import Widget
 from widgets.docks import DockWidget
-from widgets.labels import LCDValueWithTitle
+from widgets.labels import LCDValueWithTitle, LCDIntWithTitle
 
 
 class DockTemplate(DockWidget):
     def __init__(self, title: str):
         super().__init__(title)
-        # 現在株価表示
+        # 現在株価
         price = LCDValueWithTitle("現在株価")
         self.layout.addWidget(price)
+        # 含み損益
+        profit = LCDValueWithTitle("含み損益")
+        self.layout.addWidget(profit)
+        # 合計収益
+        total = LCDValueWithTitle("合計収益")
+        self.layout.addWidget(total)
+        # EP 更新回数
+        epupd = LCDIntWithTitle("EP 更新回数")
+        self.layout.addWidget(epupd)
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Dock Template")
-        ticker = "ABCD"
+        ticker = "Ticker"
         dock = DockTemplate(ticker)
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
 
