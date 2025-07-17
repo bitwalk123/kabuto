dadde26a6edf8b5370ee752209d97b56cd89be58
Fuhito Suguri <bitwalk@gmail.com>
2025-07-18 08:54:10 +0900

    update

--- samples/sample_029_dock_template.py
+++ samples/sample_029_dock_template.py
@@ -6,9 +6,34 @@ from PySide6.QtWidgets import (
     QMainWindow,
 )
 
-from widgets.containers import Widget
+from widgets.buttons import ButtonBuy, ButtonSell
+from widgets.containers import Widget, PadH
 from widgets.docks import DockWidget
 from widgets.labels import LCDValueWithTitle, LCDIntWithTitle
+from widgets.layouts import VBoxLayout, HBoxLayout
+
+
+class PanelTrading(Widget):
+    def __init__(self):
+        super().__init__()
+        layout = VBoxLayout()
+        self.setLayout(layout)
+
+        # 一行用レイアウト
+        layout_row = HBoxLayout()
+        layout.addLayout(layout_row)
+
+        # 売建
+        self.sell = but_sell = ButtonSell()
+        layout_row.addWidget(but_sell)
+
+        # 余白
+        pad = PadH()
+        layout_row.addWidget(pad)
+
+        # 買建
+        self.buy = but_buy = ButtonBuy()
+        layout_row.addWidget(but_buy)
 
 
 class DockTemplate(DockWidget):
@@ -26,6 +51,10 @@ class DockTemplate(DockWidget):
         # EP 更新回数
         epupd = LCDIntWithTitle("EP 更新回数")
         self.layout.addWidget(epupd)
+
+        # 取引用パネル
+        trading = PanelTrading()
+        self.layout.addWidget(trading)
 
 
 class Example(QMainWindow):
