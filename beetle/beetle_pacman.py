5b233d625f0ed21bdeb7ccdbb30649410503dbe7
Fuhito Suguri <bitwalk@gmail.com>
2025-07-20 15:52:48 +0900

    update

--- beetle/beetle_pacman.py
+++ beetle/beetle_pacman.py
@@ -7,33 +7,34 @@ class PacMan:
     """
 
     def __init__(self):
-        self.counter = 0  # トータルカウンター
-        self.sar = 0  # 反転カウンター
+        self.counter: int = 0  # トータルカウンター
+        self.sar: int = 0  # 反転カウンター
         self.trend: int = 0  # トレンドの向き
-        self.epupd: int = 0  # 更新回数
-        self.num = 1
-        self.has_position = False
+        self.epupd_min: int = 1  # トレンド追従を開始するための EP 更新回数の最低回数
+        self.has_position: bool = False  # ポジションを持っているか？
 
     def setTrend(self, trend: int, epupd: int) -> PositionType:
+        # カウンタの更新
         self.counter += 1
-        self.epupd = epupd
-        if self.trend != trend:
-            if self.trend * trend == -1:
-                ptype = PositionType.REPAY
+
+        if self.trend != trend: # トレンド反転時
+            if self.trend * trend == -1: # トレンドが -1 と 1 の時
+                self.trend = trend
                 self.has_position = False
-            else:
-                ptype = PositionType.NONE
-            self.trend = trend
+                return PositionType.REPAY
+            else: # 最初のトレンド 0 から -1 あるいは 1 へ変更した時
+                self.trend = trend
+                return PositionType.NONE
         else:
-            if self.num < epupd and not self.has_position:
-                if self.trend == 1:
-                    ptype = PositionType.BUY
+            if self.epupd_min < epupd and not self.has_position:
+                # 建玉無しで EP 更新回数が必要最低回数より大きくなった時
+                if 0 < self.trend:
                     self.has_position = True
-                elif self.trend == -1:
-                    ptype = PositionType.SELL
+                    return PositionType.BUY
+                elif self.trend < 0:
                     self.has_position = True
+                    return PositionType.SELL
                 else:
-                    ptype = PositionType.NONE
+                    return PositionType.NONE
             else:
-                ptype = PositionType.NONE
-        return ptype
+                return PositionType.NONE
