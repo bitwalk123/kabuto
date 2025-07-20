c5ddb3853db84ef1f43c93af8957087f473c2d31
Fuhito Suguri <bitwalk@gmail.com>
2025-07-20 15:18:39 +0900

    update

--- beetle/beetle_pacman.py
+++ beetle/beetle_pacman.py
@@ -12,6 +12,7 @@ class PacMan:
         self.trend: int = 0  # トレンドの向き
         self.epupd: int = 0  # 更新回数
         self.num = 1
+        self.has_position = False
 
     def setTrend(self, trend: int, epupd: int) -> PositionType:
         self.counter += 1
@@ -19,15 +20,18 @@ class PacMan:
         if self.trend != trend:
             if self.trend * trend == -1:
                 ptype = PositionType.REPAY
+                self.has_position = False
             else:
                 ptype = PositionType.NONE
             self.trend = trend
         else:
-            if epupd == self.num:
+            if self.num < epupd and not self.has_position:
                 if self.trend == 1:
                     ptype = PositionType.BUY
+                    self.has_position = True
                 elif self.trend == -1:
                     ptype = PositionType.SELL
+                    self.has_position = True
                 else:
                     ptype = PositionType.NONE
             else:
