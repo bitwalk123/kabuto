a92697212dd7cee2971203ddfc7cb3c74ba43926
Fuhito Suguri <bitwalk@gmail.com>
2025-07-25 10:53:55 +0900

    update

--- rhino/rhino_main.py
+++ rhino/rhino_main.py
@@ -7,9 +7,15 @@ from PySide6.QtCore import QTimer
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QMainWindow
 
+from funcs.uis import clear_boxlayout
+from modules.trans import WinTransaction
 from rhino.rhino_funcs import get_intraday_timestamp
+from rhino.rhino_psar import PSARObject
 from rhino.rhino_review import RhinoReview
+from rhino.rhino_ticker import ThreadTicker
 from rhino.rhino_toolbar import RhinoToolBar
+from rhino.rhino_trader import RhinoTrader
+from structs.posman import PositionType
 from structs.res import AppRes
 from widgets.containers import Widget
 from widgets.layouts import VBoxLayout
@@ -56,8 +62,23 @@ class Rhino(QMainWindow):
         # スレッド用インスタンス
         self.review: RhinoReview | None = None
 
+        # システム時刻（タイムスタンプ）
+        self.ts_system = 0
+
         # ザラ場の開始時間などのタイムスタンプ取得（本日分）
         self.dict_ts = get_intraday_timestamp()
+
+        # trader インスタンスを保持する辞書
+        self.dict_trader = dict()
+
+        # ThreadTicker 用インスタンス
+        self.thread_ticker: ThreadTicker | None = None
+        # ThreadTicker インスタンスを保持する辞書
+        self.dict_thread_ticker = dict()
+
+        # 取引履歴
+        self.df_transaction: pd.DataFrame | None = None
+        self.win_transaction: WinTransaction | None = None
 
         # ---------------------------------------------------------------------
         #  UI
@@ -84,6 +105,57 @@ class Rhino(QMainWindow):
         self.timer = timer = QTimer()
         timer.setInterval(self.timer_interval)
 
+    def create_trader(self, list_ticker, dict_name, dict_lastclose):
+        """
+        銘柄数分の Trader インスタンスの生成
+        （リアルタイム・モード、デバッグ・モード共通）
+        :param list_ticker:
+        :param dict_name:
+        :param dict_lastclose:
+        :return:
+        """
+        # 配置済みの Trader インスタンスを消去
+        clear_boxlayout(self.layout)
+        # Trader 辞書のクリア
+        self.dict_trader = dict()
+        # Thread ticker インスタンスをクリア
+        self.dict_thread_ticker = dict()
+
+        # 銘柄数分の Trader インスタンスの生成
+        for ticker in list_ticker:
+            # Trader インスタンスの生成
+            trader = RhinoTrader(self.res, ticker)
+            # Dock の売買ボタンのクリック・シグナルを直接ハンドリング
+            if debug:
+                trader.dock.clickedBuy.connect(self.on_buy_review)
+                trader.dock.clickedRepay.connect(self.on_repay_review)
+                trader.dock.clickedSell.connect(self.on_sell_review)
+
+            # Trader 辞書に保持
+            self.dict_trader[ticker] = trader
+
+            # 「銘柄名　(ticker)」をタイトルにして設定し直し
+            trader.setChartTitle(f"{dict_name[ticker]} ({ticker})")
+
+            # 当日ザラ場時間
+            trader.setTimeAxisRange(self.dict_ts["start"], self.dict_ts["end"])
+
+            # 前日終値
+            if dict_lastclose[ticker] > 0:
+                trader.setLastCloseLine(dict_lastclose[ticker])
+
+            # 配置
+            self.layout.addWidget(trader)
+
+            # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+            # Thread Ticker
+            # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+            self.thread_ticker = thread_ticker = ThreadTicker(ticker)
+            thread_ticker.threadReady.connect(self.on_thread_ticker_ready)
+            thread_ticker.worker.notifyPSAR.connect(self.on_update_psar)
+            thread_ticker.start()
+            self.dict_thread_ticker[ticker] = thread_ticker
+
     def on_thread_finished(self, result: bool):
         """
         スレッド終了時のログ
@@ -98,6 +170,9 @@ class Rhino(QMainWindow):
         if self.timer.isActive():
             self.timer.stop()
             self.logger.info("タイマーを停止しました。")
+
+    def on_thread_ticker_ready(self, ticker: str):
+        self.logger.info(f"Thread for {ticker} is ready.")
 
     def on_transaction_result(self, df: pd.DataFrame):
         """
@@ -125,7 +200,6 @@ class Rhino(QMainWindow):
         for ticker in dict_data.keys():
             x, y = dict_data[ticker]
             trader = self.dict_trader[ticker]
-            # trader.setTimePrice(x, y)
             trader.dock.setPrice(y)
             # 銘柄単位の含み益と収益を更新
             trader.dock.setProfit(dict_profit[ticker])
@@ -134,6 +208,44 @@ class Rhino(QMainWindow):
             thread_ticker: ThreadTicker = self.dict_thread_ticker[ticker]
             # ここで PSAR を算出する処理が呼び出される
             thread_ticker.notifyNewPrice.emit(x, y)
+
+    def on_update_psar(self, ticker: str, x: float, ret: PSARObject):
+        """
+        Parabolic SAR のトレンド点を追加
+        :param ticker:
+        :param x:
+        :param ret:
+        :return:
+        """
+        trader: RhinoTrader = self.dict_trader[ticker]
+        trader.setPlotData(x, ret)
+
+    # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+    # 取引ボタンがクリックされた時の処理（レビュー用）
+    # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+    def on_buy_review(self, ticker: str, price: float, note: str):
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # 🧿 買建で建玉取得をワーカースレッドに通知
+        self.review.requestPositionOpen.emit(
+            ticker, self.ts_system, price, PositionType.BUY, note
+        )
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    def on_sell_review(self, ticker: str, price: float, note: str):
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # 🧿 売建で建玉取得をワーカースレッドに通知
+        self.review.requestPositionOpen.emit(
+            ticker, self.ts_system, price, PositionType.SELL, note
+        )
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    def on_repay_review(self, ticker: str, price: float, note: str):
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # 🧿 建玉返済をワーカースレッドに通知
+        self.review.requestPositionClose.emit(
+            ticker, self.ts_system, price, note
+        )
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
     # デバッグ用メソッド
---
+++ rhino/rhino_pacman.py
@@ -0,0 +1,43 @@
+from beetle.beetle_psar import PSARObject
+from structs.posman import PositionType
+
+
+class PacMan:
+    """
+    トレンド判定ロジック
+    """
+
+    def __init__(self):
+        self.counter: int = 0  # トータルカウンター
+        self.sar: int = 0  # 反転カウンター
+        self.trend: int = 0  # トレンドの向き
+        self.epupd_min: int = 10  # トレンド追従を開始するための EP 更新回数の最低回数
+        self.has_position: bool = False  # ポジションを持っているか？
+
+    def setTrend(self, ret: PSARObject) -> PositionType:
+        # カウンタの更新
+        self.counter += 1
+
+        if self.trend != ret.trend:  # トレンド反転時
+            if self.trend * ret.trend == -1:  # トレンドが -1 と 1 の時
+                self.trend = ret.trend
+                self.has_position = False  # ポジションをリセット
+                return PositionType.REPAY
+            else:  # 最初のトレンド 0 から -1 あるいは 1 へ変更した時
+                self.trend = ret.trend
+                self.has_position = False  # ポジションをリセット
+                return PositionType.NONE
+        else:
+            if not self.has_position and self.epupd_min < ret.epupd:
+                # 建玉無しで EP 更新回数が必要最低回数より大きくなった時
+                if 0 < self.trend:
+                    self.has_position = True
+                    return PositionType.BUY
+                elif self.trend < 0:
+                    self.has_position = True
+                    return PositionType.SELL
+                else:
+                    self.has_position = False
+                    return PositionType.NONE
+            else:
+                return PositionType.HOLD
