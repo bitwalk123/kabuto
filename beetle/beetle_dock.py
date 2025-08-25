b2f9534a683cf15d6cf35b98f5b793449f4eba7f
Fuhito Suguri <bitwalk@gmail.com>
2025-08-25 16:29:36 +0900

    update

--- beetle/beetle_dock.py
+++ beetle/beetle_dock.py
@@ -3,7 +3,6 @@ import logging
 from PySide6.QtCore import Signal
 
 from modules.panel import PanelTrading
-from structs.app_enum import PositionType
 from structs.res import AppRes
 from widgets.docks import DockWidget
 from widgets.labels import LCDValueWithTitle
