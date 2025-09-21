bf3326debb3f330998033a148c3881f2b4e4e6fc
Fuhito Suguri <bitwalk@gmail.com>
2025-09-22 05:14:36 +0900

    update

--- modules/trading_env.py
+++ modules/trading_env.py
@@ -24,7 +24,7 @@ class TransactionManager:
     def __init__(self):
         # modified on 20250921
         self.bonus_contract = -0.1  # 約定ボーナスまたはペナルティ（買建、売建、返済）
-        self.reward_pnl_scale = 0.5  # 含み損益のスケール（含み損益✕係数）
+        self.reward_pnl_scale = 0.75  # 含み損益のスケール（含み損益✕係数）
         self.reward_hold = 0.1  # 建玉を保持する報酬
         self.penalty_none = +0.05  # 建玉を持たないペナルティ
         self.penalty_rule = -1.0  # 売買ルール違反
