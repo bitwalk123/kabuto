e9c4aa26ef488278b1ae2634bb21322a2ac52ffb
Fuhito Suguri <bitwalk@gmail.com>
2025-09-22 07:02:07 +0900

    update

--- modules/trading_env.py
+++ modules/trading_env.py
@@ -264,10 +264,10 @@ class TradingEnv(gym.Env):
         self.df[colname] = (ta.RSI(mva, period - 1) - 50.) / 100.
         list_features.append(colname)
 
-        # 6. TRIX(ROC)
-        colname = "TRIX"
+        # 6. ROC → 取引数が増える！
+        colname = "ROC"
         mva = self.df["Price"].rolling(period, min_periods=1).median()
-        self.df[colname] = ta.TRIX(mva, 30)
+        self.df[colname] = ta.ROC(mva, period - 1)
         list_features.append(colname)
         """
 
