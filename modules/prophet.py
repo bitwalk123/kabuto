6ddf8803bb80a7b7d3fb8fc2c03fb1929a933b7d
Fuhito Suguri <bitwalk@gmail.com>
2025-12-05 16:54:53 +0900

    update

--- modules/prophet.py
+++ modules/prophet.py
@@ -234,7 +234,7 @@ class Prophet(QMainWindow):
             ■■■ DOE 用 ■■■
             ティックファイル・リスト
             """
-            self.list_tick = self.toolbar.getListTicks(reverse=False)[-1:]
+            self.list_tick = self.toolbar.getListTicks(reverse=False)
             self.idx_tick = 0
             self.start_mode_doe()
         else:
