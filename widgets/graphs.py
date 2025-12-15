7ba1465344511b210ca0e448cfbbb590f5a6e995
Fuhito Suguri <bitwalk@gmail.com>
2025-12-15 13:45:33 +0900

    update

--- widgets/graphs.py
+++ widgets/graphs.py
@@ -46,23 +46,23 @@ class TrendGraph(pg.PlotWidget):
         generic_monospace_font_small.setPointSize(10)  # サイズ設定
 
         # ★★★ X軸のティックラベルのフォントを設定 ★★★
-        #axis_x_item = self.getAxis('bottom')
-        #font_x = QFont("monospace")
+        # axis_x_item = self.getAxis('bottom')
+        # font_x = QFont("monospace")
 
         # font_x.setPointSize(10)
-        #axis_x_item.tickFont = font_x  # setLabelFontではなくtickFont属性に直接代入
+        # axis_x_item.tickFont = font_x  # setLabelFontではなくtickFont属性に直接代入
         self.getAxis('bottom').setTickFont(generic_monospace_font_small)
-        #axis_x_item.setStyle(tickTextOffset=8)  # フォント変更後、必要に応じてオフセットを調整
+        # axis_x_item.setStyle(tickTextOffset=8)  # フォント変更後、必要に応じてオフセットを調整
 
         # ★★★ Y軸のティックラベルのフォントを設定 ★★★
-        #axis_y_item = self.getAxis('left')
-        #font_y = QFont("monospace")
+        # axis_y_item = self.getAxis('left')
+        # font_y = QFont("monospace")
 
         # font_y.setPointSize(10)
-        #axis_y_item.tickFont = font_y  # setLabelFontではなくtickFont属性に直接代入
+        # axis_y_item.tickFont = font_y  # setLabelFontではなくtickFont属性に直接代入
         # QFont.Monospace をY軸ティックラベルに適用
         self.getAxis('left').setTickFont(generic_monospace_font_small)
-        #axis_y_item.setStyle(tickTextOffset=8)  # フォント変更後、必要に応じてオフセットを調整
+        # axis_y_item.setStyle(tickTextOffset=8)  # フォント変更後、必要に応じてオフセットを調整
 
 
 class TrendGraph2(TrendGraph):
