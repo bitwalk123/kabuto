eb7c24d92cdacdb81323ba163cc9c196ca6fd8c0
Fuhito Suguri <bitwalk@gmail.com>
2025-12-30 15:04:41 +0900

    update

--- modules/feature_provider.py
+++ modules/feature_provider.py
@@ -121,15 +121,15 @@ class FeatureProvider:
         移動平均差 (Moving Average Difference = MAD) の算出
         :return:
         """
-        mad_new = self.ma_1 - self.ma_2
-        if 0 < mad_new:
-            signal_sign_new = SignalSign.POSITIVE
-        elif mad_new < 0:
-            signal_sign_new = SignalSign.NEGATIVE
+        mad = self.ma_1 - self.ma_2
+        if 0 < mad:
+            signal_sign = SignalSign.POSITIVE
+        elif mad < 0:
+            signal_sign = SignalSign.NEGATIVE
         else:
-            signal_sign_new = SignalSign.ZERO
+            signal_sign = SignalSign.ZERO
 
-        return mad_new, signal_sign_new
+        return mad, signal_sign
 
     def _calc_mr(self) -> float:
         """
@@ -173,32 +173,6 @@ class FeatureProvider:
             return 1
         else:
             return 0
-
-    '''
-    def doesTakeProfit(self) -> bool:
-        """
-        利確
-        :return:
-        """
-        profit = self.get_profit()
-        if 50 < self.profit_max:
-            if profit < self.profit_max * 0.9:
-                return True
-            else:
-                return False
-        elif 20 < self.profit_max:
-            if profit < self.profit_max * 0.8:
-                return True
-            else:
-                return False
-        elif 10 < self.profit_max:
-            if profit < self.profit_max * 0.3:
-                return True
-            else:
-                return False
-        else:
-            return False
-    '''
 
     def beta_ratio(
             self,
