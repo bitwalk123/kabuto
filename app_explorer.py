b094adaef35be744b35266e2c203930750bd8660
Fuhito Suguri <bitwalk@gmail.com>
2026-01-20 16:47:14 +0900

    update

--- app_explorer.py
+++ app_explorer.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     main_logger = setup_logging()
     res = AppRes()
 
-    # list_doe = ["doe-13c"]
-    list_doe = ["doe-14a"]
+    list_doe = ["doe-13d"]
+    #list_doe = ["doe-14a"]
     for name_doe in list_doe:
         list_code = ["7011", "7203", "8306"]
         for code in list_code:
