6c33009cbe48488f9ec77d48ba47125fb0a0b41c
Keiichi Takahashi <bitwalk@gmail.com>
2026-01-21 18:19:30 +0900

    update

--- snippet/simple_xlwings.py
+++ snippet/simple_xlwings.py
@@ -2,5 +2,5 @@ import xlwings as xw
 
 if __name__ == "__main__":
     wb = xw.Book()  # 新しいワークブックを開く
-    sheet = wb.sheets["Sheet1"] # シート・オブジェクトをインスタンス化
-    sheet["A1"].value = "Python から書き込みました。" # 値の書き込み
+    sheet = wb.sheets["Sheet1"]  # シート・オブジェクトをインスタンス化
+    sheet["A1"].value = "Python から書き込みました。"  # 値の書き込み
