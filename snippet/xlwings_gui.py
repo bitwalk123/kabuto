47fde0db7f3b4bc639546f9f119f44dcd2985a99
Fuhito Suguri <bitwalk@gmail.com>
2026-01-21 19:12:46 +0900

    update

---
+++ snippet/xlwings_gui.py
@@ -0,0 +1,74 @@
+import sys
+
+import xlwings as xw
+from PySide6.QtCore import (
+    QObject,
+    QThread,
+    Signal,
+)
+from PySide6.QtGui import QCloseEvent
+from PySide6.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class ExcelWorker(QObject):
+    def __init__(self):
+        super().__init__()
+        self.sheet = None
+
+    def initWorker(self):
+        wb = xw.Book()  # 新しいワークブックを開く
+        self.sheet = wb.sheets["Sheet1"]  # シート・オブジェクトをインスタンス化
+
+    def excel_write(self, msg: str):
+        self.sheet["A1"].value = msg  # 値の書き込み
+
+
+class SampleXlwings(QWidget):
+    requestWorkerInit = Signal()
+    requestWrite = Signal(str)
+
+    def __init__(self):
+        super().__init__()
+        # xlwings用スレッド
+        self.thread = thread = QThread(self)
+        self.worker = worker = ExcelWorker()
+        worker.moveToThread(thread)
+        thread.started.connect(self.requestWorkerInit.emit)
+        self.requestWorkerInit.connect(worker.initWorker)
+        self.requestWrite.connect(worker.excel_write)
+        thread.start()
+        # GUI
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        but = QPushButton("新規 Excel へ文字列を書き込む")
+        but.clicked.connect(self.request_write)
+        layout.addWidget(but)
+
+    def closeEvent(self, event: QCloseEvent):
+        if self.thread is not None:
+            self.thread.quit()
+            self.thread.wait()
+        if self.worker is not None:
+            self.worker.deleteLater()
+            self.worker = None
+        event.accept()
+
+    def request_write(self):
+        msg = "Python から書き込みました。"
+        self.requestWrite.emit(msg)
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = SampleXlwings()
+    win.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
