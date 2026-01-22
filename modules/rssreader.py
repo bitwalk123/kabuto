9768d8e07a5379fcb9252776bd1a89765b36b318
Fuhito Suguri <bitwalk@gmail.com>
2026-01-22 13:30:51 +0900

    update

--- modules/rssreader.py
+++ modules/rssreader.py
@@ -142,8 +142,8 @@ class RSSReaderWorker(QObject):
 
         # 一括読み取り対象の行範囲を取得
         rows = [self.dict_row[code] for code in self.list_code]
-        self.min_row = min(rows) + 1
-        self.max_row = max(rows) + 1
+        self.min_row = min(rows)
+        self.max_row = max(rows)
 
         # 銘柄別に空の辞書/リストを準備 → あとでデータフレームに変換
         for code in self.list_code:
@@ -247,6 +247,7 @@ class RSSReaderWorker(QObject):
             for i, code in enumerate(self.list_code):
                 price = prices[i]
                 volume = volumes[i]
+                print(price)
                 if price > 0:
                     self.dict_data[code] = (ts, price, volume)
                     self.dict_profit[code] = self.posman.getProfit(code, price)
