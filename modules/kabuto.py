bae24ac1fbedc19349b8b984ab40e54d6a831c83
Fuhito Suguri <bitwalk@gmail.com>
2026-02-02 16:22:39 +0900

    update

--- modules/kabuto.py
+++ modules/kabuto.py
@@ -39,7 +39,7 @@ from widgets.layouts import VBoxLayout
 
 class Kabuto(QMainWindow):
     __app_name__ = "Kabuto"
-    __version__ = "0.3.3"
+    __version__ = "0.3.4"
     __author__ = "Fuhito Suguri"
     __license__ = "MIT"
 
