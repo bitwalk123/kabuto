019dfa409ef1e13b123ecf5935451dc9490962c8
Fuhito Suguri <bitwalk@gmail.com>
2026-02-11 11:03:27 +0900

    update

--- modules/algo_trade.py
+++ modules/algo_trade.py
@@ -1,9 +1,9 @@
-from structs.app_enum import SignalSign, ActionType, PositionType
+from structs.app_enum import ActionType, PositionType
 
 
 class AlgoTrade:
     """
-    強化学習モデルの代わりに、自作のアルゴリズムで取引するクラス
+    強化学習モデルの代わりに、自作のアルゴリズムで取引する疑似モデルのクラス
     """
 
     def __init__(self):
@@ -18,10 +18,24 @@ class AlgoTrade:
 
     @staticmethod
     def can_execute(action, masks):
+        """
+        アクションが行動マスクで禁止されていないかチェック
+        :param action:
+        :param masks:
+        :return:
+        """
         return masks[action] == 1
 
     @staticmethod
     def exit_action(position: PositionType):
+        """
+        ポジションに応じた返済アクション
+        【備考】
+        以前は返済アクションにREPAYMENTを用意していたが、
+        モデルの学習に合わないので削除した。→ 疑似モデルでも維持
+        :param position:
+        :return:
+        """
         if position == PositionType.LONG:
             return ActionType.SELL.value
         if position == PositionType.SHORT:
@@ -61,6 +75,11 @@ class AlgoTrade:
         return ActionType.HOLD.value, {}
 
     def updateObs(self, list_obs_label):
+        """
+        疑似ロジックでは、観測値にラベルを付けておかないと、コーディングする側が間違える！
+        :param list_obs_label:
+        :return:
+        """
         self.list_obs_label = list_obs_label
         self.idx_cross_1 = self.list_obs_label.index("クロスS1")
         self.idx_losscut_1 = self.list_obs_label.index("ロス1")
