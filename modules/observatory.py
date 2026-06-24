91e0b81402267b272c8d48c80cb9159c133280f0
Fuhito Suguri <bitwalk@gmail.com>
2026-06-24 19:08:56 +0900

    update

--- modules/observatory.py
+++ modules/observatory.py
@@ -1,8 +1,8 @@
 from modules.env_data import EnvData
 from modules.technical import (
-    HMA,
     Momentum,
     MovingAverage,
+    PurePursuitFollower,
     VWAP,
 )
 
@@ -13,8 +13,8 @@ class ObservationManager:
         self.s = s
         # 特徴量インスタンス
         # self.ma_1 = MovingAverage(window_size=self.s.PERIOD_MA_1)
-        # self.ma_1 = PurePursuitFollower()
-        self.ma_1 = HMA(window_size=self.s.PERIOD_MA_1)
+        self.ma_1 = PurePursuitFollower()
+        #self.ma_1 = HMA(window_size=self.s.PERIOD_MA_1)
         self.ma_2 = MovingAverage(window_size=self.s.PERIOD_MA_2)
         # self.ma_2 = HMA(window_size=self.s.PERIOD_MA_2)
         self.mom = Momentum(window_size=self.s.PERIOD_MOM)
