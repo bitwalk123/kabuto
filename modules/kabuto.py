d8479ee9bb59dd78086fdada99804638852bf26d
Fuhito Suguri <bitwalk123@users.noreply.github.com>
2026-09-16 16:00:27 +0900

    update

--- modules/kabuto.py
+++ modules/kabuto.py
@@ -36,7 +36,7 @@ from widgets.statusbars import StatusBar
 
 class Kabuto(QMainWindow):
     __app_name__ = "Kabuto"
-    __version__ = "0.8.32"
+    __version__ = "0.8.33"
     __author__ = "Fuhito Suguri"
     __license__ = "MIT"
 
